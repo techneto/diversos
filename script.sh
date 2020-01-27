213510e572bb1f8751f63e2e58366148edb80c70
Joaquim Neto <47262782+techneto@users.noreply.github.com>
2020-01-27 13:07:11 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,29 @@
-#cp desenhando.svg 01.svg
-#sed -i 's/$nome/Joaquim Neto/g' 01.svg 
-#sed -i 's/$profissao/TECNICO EM REDES/g' 01.svg 
-#sed -i 's/01\.jpeg/02\.png/g' 01.svg
+#!/bin/bash
+
+#
+#AUTHOR JOAQUIM BARTOLOMEU FERREIRA NETO
+#MEU CANAL NO YOUTUBE https://www.youtube.com/channel/UCQeu-TOpmVrZSFSPUN65BPA
+#
+
+############### OBSERVAÇÕES ###################
+#ARQUIVOS DE EXEMPLO DISPONIBLIZADOS, O DADOS.CSV É UMA
+#PLANILHA COM AS INFORMAÇÕES DOS ARQUIVOS QUE SERÃO GERADOS COM O SCRIPT
+
+#ARQUIVOS MODELO FEITO NO INKSCAPE E COLOCADOS DENTRO DO MESMO DIRETÓRIO DESENHANDO.SVG
+	
+#VOCÊ PODE FAZER UM ARQUIVO CSV COM ALGUM EDITOR DE PLANILHAS
 
 while read linha
 	do
-#		echo $linha| cut -d , -f 1
-		cp desenhando.svg $(echo $linha | cut -d , -f 4).svg
+		#COPIA O ARQUIVO MODELO COM A ID DA PLANILHA
+		cp desenhando.svg $(echo $linha | cut -d , -f 4).svg 
+		
+		#SUBSTITUI O NOME DO ARQUIVO PELO ARQUIVO DA PLANILHA
 		sed -i "s/\$nome/$(echo $linha | cut -d , -f 1)/g" $(echo $linha | cut -d , -f 4).svg
+		
+		#SUBSTITUI A PROFISSÃO PELA PROFISSÃO DA PLANILHA
 		sed -i "s/\$profissao/$(echo $linha | cut -d , -f 2)/g" $(echo $linha | cut -d , -f 4).svg
+		
+		#SUBSTITUI O ARQUIVO DA FOTO PELA FOTO DA PLANILHA
 		sed -i "s/01\.jpeg/$(echo $linha | cut -d , -f 3)/g" $(echo $linha | cut -d , -f 4).svg
-	done < dados.csv
-echo "Modificações feitas com suscesso!"
+	done < dados.csv #ARQUIVO QUE SERÁ FEITO O LOOPING
